f20735e1f9e51fae344d1177146bdbff0b9c176d
Mahdi Najjarian <mnajarian@gmail.com>
2018-01-15 22:45:07 +0200

    Update and rename Self-Check 1.10: Shaq to Self-Check 1.10: Shaq.java

--- Chapter 1- Introduction to Java Programming/Self-Check 1.10: Shaq.java
+++ Chapter 1- Introduction to Java Programming/Self-Check 1.10: Shaq.java
@@ -1,10 +1,9 @@
-What is the output produced from the following statements?
+/*What is the output produced from the following statements?
 
 System.out.println("Shaq is 7'1");
 System.out.println("The string \"\" is an empty message.");
 System.out.println("\\'\"\"");
-------------------------------------------
-output:
+*/
 Shaq is 7'1
 The string "" is an empty message.
 \'""
