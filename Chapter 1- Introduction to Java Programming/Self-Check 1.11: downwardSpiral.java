a8333dd4cd097b0a9cec34f7270c95d7f17726c0
Mahdi Najjarian <mnajarian@gmail.com>
2018-01-15 22:45:45 +0200

    Update and rename Self-Check 1.11: downwardSpiral to Self-Check 1.11: downwardSpiral.java

--- Chapter 1- Introduction to Java Programming/Self-Check 1.11: downwardSpiral.java
+++ Chapter 1- Introduction to Java Programming/Self-Check 1.11: downwardSpiral.java
@@ -1,12 +1,11 @@
-What is the output produced from the following statements? (Treat tabs as aligning to every multiple of eight spaces.)
+/*What is the output produced from the following statements? (Treat tabs as aligning to every multiple of eight spaces.)
 
 System.out.println("\ta\tb\tc");
 System.out.println("\\\\");
 System.out.println("'");
 System.out.println("\"\"\"");
 System.out.println("C:\nin\the downward spiral");
----------------------------------------------------
-output:
+*/
         a       b       c
 \\
 '
