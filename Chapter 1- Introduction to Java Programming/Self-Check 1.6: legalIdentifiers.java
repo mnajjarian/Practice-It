6d2fc5d6f27d8f3e1f65d9a354dbe8a62b744e30
Mahdi Najjarian <mnajarian@gmail.com>
2018-01-15 23:07:07 +0200

    Update and rename Self-Check 1.6: legalIdentifiers to Self-Check 1.6: legalIdentifiers.java

---
+++ Chapter 1- Introduction to Java Programming/Self-Check 1.6: legalIdentifiers.java
@@ -0,0 +1,18 @@
+/*Which of the following can be used in a Java program as identifiers? Check all of the identifiers that are legal.
+  ABC
+  for
+  B4
+  _average
+  42isThesolution
+  sum_of_data
+  println
+  first-name
+  AnnualSalary
+  "hello"
+*/
+  ABC
+  B4
+  _average
+  sum_of_data
+  println
+  AnnualSalary
