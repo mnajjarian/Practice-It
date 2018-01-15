0e7bd53087f341342eede0e849db90a020c708a3
Mahdi Najjarian <mnajarian@gmail.com>
2018-01-15 22:36:05 +0200

    Update and rename Exercise 1.11: TwoRockets to Exercise 1.11: TwoRockets.java

--- Chapter 1- Introduction to Java Programming/Exercise 1.11: TwoRockets.java
+++ Chapter 1- Introduction to Java Programming/Exercise 1.11: TwoRockets.java
@@ -1,4 +1,4 @@
-Write a complete Java program in a class named TwoRockets that generates the following output. Use static methods to 
+/*Write a complete Java program in a class named TwoRockets that generates the following output. Use static methods to 
 show structure and eliminate redundancy in your solution.
 
 Note that there are two rocket ships next to each other. What redundancy can you eliminate using static methods? 
@@ -21,9 +21,7 @@ What redundancy cannot be eliminated?
   /  \     /  \
  /    \   /    \
 
------------------------------------------------------
-Type your solution here:
-
+*/
 public class TwoRockets {
     public static void main(String[] args) {
         cap();
