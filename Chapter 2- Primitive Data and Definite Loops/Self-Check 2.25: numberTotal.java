5ef577b132046249d78b44e308d35e2b24c4f277
Mahdi Najjarian <mnajarian@gmail.com>
2018-01-15 22:16:02 +0200

    Update Self-Check 2.25: numberTotal.java

--- Chapter 2- Primitive Data and Definite Loops/Self-Check 2.25: numberTotal.java
+++ Chapter 2- Primitive Data and Definite Loops/Self-Check 2.25: numberTotal.java
@@ -1,12 +1,13 @@
 /*What is the output of the following loop?
-*int total = 25;
-*for (int number = 1; number <= (total / 2); number++) {
-    *total = total - number;
-    *System.out.println(total + " " + number);
-}*/
+int total = 25;
+for (int number = 1; number <= (total / 2); number++) {
+    total = total - number;
+    System.out.println(total + " " + number);
+}
+*/
 
 
-//output:
+output:
       24 1
       22 2
       19 3
