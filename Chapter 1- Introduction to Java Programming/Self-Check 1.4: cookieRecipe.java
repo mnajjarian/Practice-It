373ae14de533ee15c8756d7cf2e775b2a050af25
Mahdi Najjarian <mnajarian@gmail.com>
2018-01-15 23:04:22 +0200

    Update and rename Self-Check 1.4: cookieRecipe to Self-Check 1.4: cookieRecipe.java

--- Chapter 1- Introduction to Java Programming/Self-Check 1.4: cookieRecipe.java
+++ Chapter 1- Introduction to Java Programming/Self-Check 1.4: cookieRecipe.java
@@ -1,9 +1,8 @@
-In your own words, describe an algorithm for baking cookies. Assume that you have a large number of hungry friends, 
+/*In your own words, describe an algorithm for baking cookies. Assume that you have a large number of hungry friends, 
 so you'll want to produce several batches of cookies!
 
 (Since there are so many correct answers, Practice-It will accept any solution of sufficient length.)
 
 recipe
----------------------------
-answer:
+*/
 cookieRecipe();
