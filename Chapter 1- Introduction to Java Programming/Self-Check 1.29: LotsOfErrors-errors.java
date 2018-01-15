c0bcbc91b0f38037aab829260074802cffa0f5bb
Mahdi Najjarian <mnajarian@gmail.com>
2018-01-15 23:00:25 +0200

    Update and rename Self-Check 1.29: LotsOfErrors-errors to Self-Check 1.29: LotsOfErrors-errors.java

--- Chapter 1- Introduction to Java Programming/Self-Check 1.29: LotsOfErrors-errors.java
+++ Chapter 1- Introduction to Java Programming/Self-Check 1.29: LotsOfErrors-errors.java
@@ -1,4 +1,4 @@
-The following program contains at least 10 errors. Correct the errors and submit a working version of the program.
+/*The following program contains at least 10 errors. Correct the errors and submit a working version of the program.
 
 public class LotsOf Errors {
     public static main(String args) {
@@ -9,8 +9,7 @@ public class LotsOf Errors {
     public static void message {
         System.out println("This program surely cannot ";
         System.out.println("have any "errors" in it");
-    }
-------------------------------------------------------------------
+    }*/
 public class LotsOfErrors {
     public static void main(String [] args) {
         System.out.println("Hello, world!");
