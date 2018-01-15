cd802d711b77fafa6b82fcad82d16f0ecdaad71c
Mahdi Najjarian <mnajarian@gmail.com>
2018-01-15 23:05:57 +0200

    Update and rename Self-Check 1.7: outputSyntax to Self-Check 1.7: outputSyntax.java

--- Chapter 1- Introduction to Java Programming/Self-Check 1.7: outputSyntax.java
+++ Chapter 1- Introduction to Java Programming/Self-Check 1.7: outputSyntax.java
@@ -1,10 +1,8 @@
-
-Which of the following is the correct syntax to output a message?
+/*Which of the following is the correct syntax to output a message?
   a.System.println("Hello, world!");
   b.System.out.println("Hello, world!");
   c.System.println(Hello, world!);
   d.Out.system.println"(Hello, world!)";
   e.System.println.out('Hello, world!');
---------------------------------------------------
-answer:
+*/
 b.System.out.println("Hello, world!");
