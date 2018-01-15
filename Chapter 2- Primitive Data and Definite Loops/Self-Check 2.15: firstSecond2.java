e97a0fbfdbcacb383c434462ddeb2ac2211310b2
Mahdi Najjarian <mnajarian@gmail.com>
2018-01-15 22:20:52 +0200

    Update and rename Self-Check 2.15: firstSecond2 to Self-Check 2.15: firstSecond2.java

--- Chapter 2- Primitive Data and Definite Loops/Self-Check 2.15: firstSecond2.java
+++ Chapter 2- Primitive Data and Definite Loops/Self-Check 2.15: firstSecond2.java
@@ -1,4 +1,4 @@
-Rewrite the following code from the previous exercise to be shorter, by declaring the variables together on the same line, and 
+/*Rewrite the following code from the previous exercise to be shorter, by declaring the variables together on the same line, and 
 by using the special assignment operators (e.g., +=, -=, *=, and /=) as appropriate.
 
 
@@ -6,7 +6,7 @@ int first = 8;
 int second = 19;
 first = first + second;
 second = first - second;
-first = first - second;
+first = first - second;*/
 
 shortened code:
 int first = 8, second = 19;
