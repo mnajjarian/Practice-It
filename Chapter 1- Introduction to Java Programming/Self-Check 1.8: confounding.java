98dd0b6065337bf8c5e7ecfb815a2541d9f62d4a
Mahdi Najjarian <mnajarian@gmail.com>
2018-01-15 23:05:24 +0200

    Update and rename Self-Check 1.8: confounding to Self-Check 1.8: confounding.java

--- Chapter 1- Introduction to Java Programming/Self-Check 1.8: confounding.java
+++ Chapter 1- Introduction to Java Programming/Self-Check 1.8: confounding.java
@@ -1,10 +1,9 @@
-What is the output produced from the following statements?
+/*What is the output produced from the following statements?
 
 System.out.println("\"Quotes\"");
 System.out.println("Slashes \\//");
 System.out.println("How '\"confounding' \"\\\" it is!");
----------------------------------------
-output:
+*/
 "Quotes"
 Slashes \//
 How '"confounding' "\" it is!
