8a06d6de44603eeebca19c9fd47febfc6a88ec89
Mahdi Najjarian <mnajarian@gmail.com>
2018-01-15 22:33:01 +0200

    Update and rename Self-Check 2.7: fitnessVariables to Self-Check 2.7: fitnessVariables.java

--- Chapter 2- Primitive Data and Definite Loops/Self-Check 2.7: fitnessVariables.java
+++ Chapter 2- Primitive Data and Definite Loops/Self-Check 2.7: fitnessVariables.java
@@ -1,6 +1,6 @@
-Imagine you are writing a personal fitness program that stores the user's age, gender, height (in feet or meters), and 
+/*Imagine you are writing a personal fitness program that stores the user's age, gender, height (in feet or meters), and 
 weight (to the nearest pound or kilogram). Declare variables with the appropriate names and types to hold this information. 
-Write a complete variable declaration statement with the type, the variable name, and a semicolon.
+Write a complete variable declaration statement with the type, the variable name, and a semicolon.*/
 
 age	    -------> int age;
 gender	-------> String gender;
