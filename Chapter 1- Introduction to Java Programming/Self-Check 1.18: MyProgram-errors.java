ad86bf86ee54880c1a9aed727e77316d9a18d20e
Mahdi Najjarian <mnajarian@gmail.com>
2018-01-15 22:52:05 +0200

    Update and rename Self-Check 1.18: MyProgram-errors to Self-Check 1.18: MyProgram-errors.java

--- Chapter 1- Introduction to Java Programming/Self-Check 1.18: MyProgram-errors.java
+++ Chapter 1- Introduction to Java Programming/Self-Check 1.18: MyProgram-errors.java
@@ -1,13 +1,11 @@
-The following program contains three errors. Correct the errors and submit a working version of the program.
+/*The following program contains three errors. Correct the errors and submit a working version of the program.
 
 public MyProgram {
     public static void main(String[] args) {
         System.out.println("This is a test of the")
         System.out.Println("emergency broadcast system.");
     }
-}
-----------------------------------------------------------------------------------
-answer:
+}*/
 
 public class MyProgram {
     public static void main(String[] args) {
