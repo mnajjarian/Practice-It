286482c27440e5fe435385fc799665a83da6d8bc
Mahdi Najjarian <mnajarian@gmail.com>
2018-01-15 22:32:18 +0200

    Update and rename Self-Check 2.6: declareRealNumberSyntax to Self-Check 2.6: declareRealNumberSyntax.java

---
+++ Chapter 2- Primitive Data and Definite Loops/Self-Check 2.6: declareRealNumberSyntax.java
@@ -0,0 +1,11 @@
+/*Which of the following choices is the correct syntax for declaring a real number variable named 'grade' and 
+initializing its value to 4.0?
+
+ a. double grade = 4.0;
+ b. int grade : 4.0;
+ c. grade = double 4.0;
+ d. 4.0 = grade;
+ e. grade = 4;
+*/
+ a. double grade = 4.0;
+ 
