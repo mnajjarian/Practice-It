4838f9b35f9c515d34d2e0c763f6452efdb54a8b
Mahdi Najjarian <mnajarian@gmail.com>
2018-01-15 22:24:26 +0200

    Update and rename Self-Check 2.19: ComputePay to Self-Check 2.19: ComputePay.java

--- Chapter 2- Primitive Data and Definite Loops/Self-Check 2.19: ComputePay.java
+++ Chapter 2- Primitive Data and Definite Loops/Self-Check 2.19: ComputePay.java
@@ -1,4 +1,4 @@
-The following program redundantly repeats the same expressions many times. Modify the program to remove all redundant 
+/*The following program redundantly repeats the same expressions many times. Modify the program to remove all redundant 
 expressions using variables of appropriate types. (Your variables should be used to help solve the overall problem. 
 You must use a sufficient number of variables to receive credit for solving this problem. Declare each variable 
 in its own statement on its own line.)
@@ -18,11 +18,7 @@ public class ComputePay {
         System.out.println("My taxes owed:"); // 20% tax
         System.out.println((4 + 5 + 8 + 4) * 8.75 * 0.20);
     }
-}
-
-------------------------------------------------------------------------
-
-Type your solution here:
+}*/
 
 public class ComputePay {
     public static void main(String[] args) {
