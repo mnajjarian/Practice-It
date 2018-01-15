bff23429592a1321b4a64d533c088830c442e33b
Mahdi Najjarian <mnajarian@gmail.com>
2018-01-15 22:31:04 +0200

    Update and rename Self-Check 2.4: expressions2 to Self-Check 2.4: expressions2.java

--- Chapter 2- Primitive Data and Definite Loops/Self-Check 2.4: expressions2.java
+++ Chapter 2- Primitive Data and Definite Loops/Self-Check 2.4: expressions2.java
@@ -1,5 +1,5 @@
-Trace the evaluation of the following expressions, and give their resulting values. Make sure to give a value of 
-the appropriate type (such as including a .0 at the end of a double).
+/*Trace the evaluation of the following expressions, and give their resulting values. Make sure to give a value of 
+the appropriate type (such as including a .0 at the end of a double).*/
 
 
  4.0 / 2 * 9 / 2	                            -------> 9.0
