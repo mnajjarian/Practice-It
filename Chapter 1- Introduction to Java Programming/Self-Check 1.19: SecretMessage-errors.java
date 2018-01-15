28dff3b1ffadf6ead686bb7ab43622695a50a229
Mahdi Najjarian <mnajarian@gmail.com>
2018-01-15 22:52:44 +0200

    Update and rename Self-Check 1.19: SecretMessage-errors to Self-Check 1.19: SecretMessage-errors.java

--- Chapter 1- Introduction to Java Programming/Self-Check 1.19: SecretMessage-errors.java
+++ Chapter 1- Introduction to Java Programming/Self-Check 1.19: SecretMessage-errors.java
@@ -1,11 +1,10 @@
-The following program contains four errors. Correct the errors and submit a working version of the program.
+/*The following program contains four errors. Correct the errors and submit a working version of the program.
 
 public class SecretMessage {
     public static main(string[] args) {
         System.out.println("Speak friend");
         System.out.println("and enter);
-}
--------------------------------------------------------------------------
+}*/
 public class SecretMessage {
     public static void main(String[] args) {
         System.out.println("Speak friend");
