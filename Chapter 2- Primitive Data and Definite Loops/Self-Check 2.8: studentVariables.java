3587680f6ebc72292c9a7d0a4c9389d0db85cac2
Mahdi Najjarian <mnajarian@gmail.com>
2018-01-15 22:33:43 +0200

    Update and rename Self-Check 2.8: studentVariables to Self-Check 2.8: studentVariables.java

---
+++ Chapter 2- Primitive Data and Definite Loops/Self-Check 2.8: studentVariables.java
@@ -0,0 +1,7 @@
+/*Imagine you are writing a program that stores a student's year (Freshman, Sophomore, Junior, or Senior), the number of 
+courses the student is taking, and his or her GPA on a 4.0 scale. Declare variables with the appropriate names and types to 
+hold this information. Write a complete variable declaration statement with the type, the variable name, and a semicolon.*/
+
+year	        -------> String year;
+courses taken	-------> int courses;
+GPA	          -------> double gpa;
