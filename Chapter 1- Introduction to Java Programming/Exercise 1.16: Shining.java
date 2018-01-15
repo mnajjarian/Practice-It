238f901f2bcda6a05e852c405448036e370e07e9
Mahdi Najjarian <mnajarian@gmail.com>
2018-01-15 22:39:14 +0200

    Update and rename Exercise 1.16: Shining to Exercise 1.16: Shining.java

--- Chapter 1- Introduction to Java Programming/Exercise 1.16: Shining.java
+++ Chapter 1- Introduction to Java Programming/Exercise 1.16: Shining.java
@@ -1,13 +1,10 @@
-Write a program in a class named Shining that prints the following line of output 1000 times:
+/*Write a program in a class named Shining that prints the following line of output 1000 times:
 
 All work and no play makes Jack a dull boy.
 
 You should not write a program whose source code is 1000 lines long; use methods to shorten the program. What is the shortest 
 program you can write that will produce the 1000 lines of output, using only the material from Chapter 1 (println, methods, 
-etc.)?
-
------------------------------------------------------------
-Type your solution here:
+etc.)?*/
 
 public class Shining {
 	public static void main(String[] args) {
