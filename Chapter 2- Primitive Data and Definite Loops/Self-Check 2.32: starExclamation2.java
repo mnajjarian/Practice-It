7631891b117cbf3a7c73259521ab88dbeda2d489
Mahdi Najjarian <mnajarian@gmail.com>
2018-01-17 23:13:51 +0200

    Create Self-Check 2.32: starExclamation2.java

---
+++ Chapter 2- Primitive Data and Definite Loops/Self-Check 2.32: starExclamation2.java
@@ -0,0 +1,14 @@
+/*What is the output of the following sequence of loops?
+
+
+for (int i = 1; i <= 2; i++) {
+    for (int j = 1; j <= 3; j++) {
+        for (int k = 1; k <= 4; k++) {
+            System.out.print("*");
+        }
+    }
+    System.out.print("!");
+    System.out.println();
+}*/	
+************!
+************!
