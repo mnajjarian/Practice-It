ca584ec54adcf6fec0d708e0219ea07dc67c0bea
Mahdi Najjarian <mnajarian@gmail.com>
2018-01-17 23:10:52 +0200

    Create Self-Check 2.29: multiplication.java

---
+++ Chapter 2- Primitive Data and Definite Loops/Self-Check 2.29: multiplication.java
@@ -0,0 +1,16 @@
+/*What is the output of the following sequence of loops?
+
+
+for (int i = 1; i <= 5; i++) {
+    for (int j = 1; j <= 10; j++) {
+        System.out.print((i * j) + " ");
+    }
+    System.out.println();
+}
+*/	
+1 2 3 4 5 6 7 8 9 10 
+2 4 6 8 10 12 14 16 18 20 
+3 6 9 12 15 18 21 24 27 30 
+4 8 12 16 20 24 28 32 36 40 
+5 10 15 20 25 30 35 40 45 50 
+
