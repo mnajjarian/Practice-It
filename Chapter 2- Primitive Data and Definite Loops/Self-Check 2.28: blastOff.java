87211c5211ab70c84f4f219c5717ac4958901c6f
Mahdi Najjarian <mnajarian@gmail.com>
2018-01-17 23:10:03 +0200

    Create Self-Check 2.28: blastOff.java

---
+++ Chapter 2- Primitive Data and Definite Loops/Self-Check 2.28: blastOff.java
@@ -0,0 +1,16 @@
+/*What is the output of the following loop?
+
+
+System.out.println("T-minus ");
+for (int i = 5; i >= 1; i--) {
+    System.out.println(i + ", ");
+}
+System.out.println("Blastoff!");
+*/
+T-minus 
+5, 
+4, 
+3, 
+2, 
+1,
+Blastoff!
