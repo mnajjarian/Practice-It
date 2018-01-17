49f8874dd44e95712adac378b5b0e3fcd67f8472
Mahdi Najjarian <mnajarian@gmail.com>
2018-01-17 23:09:00 +0200

    Create Self-Check 2.27: howManyLines .java

---
+++ Chapter 2- Primitive Data and Definite Loops/Self-Check 2.27: howManyLines .java
@@ -0,0 +1,12 @@
+/*What is the output of the following loop?
+
+
+for (int i = 1; i <= 3; i++)
+    System.out.println("How many lines");
+    System.out.println("are printed?");
+*/
+
+How many lines
+How many lines
+How many lines
+are printed?
