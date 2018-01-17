3445af287c89b482e9fe08d569787d1ef773bbd1
Mahdi Najjarian <mnajarian@gmail.com>
2018-01-17 23:11:50 +0200

    Create Self-Check 2.30: nestedLoops.java

---
+++ Chapter 2- Primitive Data and Definite Loops/Self-Check 2.30: nestedLoops.java
@@ -0,0 +1,26 @@
+/*What output is produced by the following program?
+
+
+public class Loops {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            for (int j = 1; j <= 10 - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}*/	
+         *
+        ***
+       *****
+      *******
+     *********
+    ***********
+   *************
+  ***************
+ *****************
+*******************
