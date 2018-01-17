b8fdc57bac5b5da813ffcafd7406d7ab49f8ded5
Mahdi Najjarian <mnajarian@gmail.com>
2018-01-17 23:07:42 +0200

    Create Self-Check 2.26: fingerTrap.java

---
+++ Chapter 2- Primitive Data and Definite Loops/Self-Check 2.26: fingerTrap.java
@@ -0,0 +1,19 @@
+/*What is the output of the following loop?
+
+
+System.out.println("+----+");
+for (int i = 1; i <= 3; i++) {
+    System.out.println("\\    /");
+    System.out.println("/    \\");
+}
+System.out.println("+----+");
+*/
+
++----+
+\    /
+/    \
+\    /
+/    \
+\    /
+/    \
++----+
