d41e64e2f6ceabe90da8b3577fe051582883f015
Mahdi Najjarian <mnajarian@gmail.com>
2018-01-24 12:27:52 +0200

    Create Exercise 2.7: spacedNumbers.java

---
+++ Chapter 2- Primitive Data and Definite Loops/Exercise 2.7: spacedNumbers.java
@@ -0,0 +1,14 @@
+/*Write nested for loops to produce the following output:
+
+    1
+   2
+  3
+ 4
+5
+*/
+for(int i = 1; i <= 5; i++) {
+for(int j = i; j <= 4; j++) {
+System.out.print(" ");
+}
+System.out.println(i);
+}
