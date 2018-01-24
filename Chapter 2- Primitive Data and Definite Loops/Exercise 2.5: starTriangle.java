27e8dbcd2ede14e01519036a2b36d64a57f07f45
Mahdi Najjarian <mnajarian@gmail.com>
2018-01-24 12:26:22 +0200

    Create Exercise 2.5: starTriangle.java

---
+++ Chapter 2- Primitive Data and Definite Loops/Exercise 2.5: starTriangle.java
@@ -0,0 +1,14 @@
+/*Write for loops to produce the following output:
+
+*
+**
+***
+****
+*****
+*/
+for(int i = 5; i >= 1; i--) {
+for(int j = i; j <= 5; j++) {
+System.out.print("*");
+}
+System.out.println();
+}
