bc4ed3472b57af96cd844ab4a79b8ec8b96d7160
Mahdi Najjarian <mnajarian@gmail.com>
2018-01-24 12:27:06 +0200

    Create Exercise 2.6: numberTriangle .java

---
+++ Chapter 2- Primitive Data and Definite Loops/Exercise 2.6: numberTriangle .java
@@ -0,0 +1,16 @@
+/*Write for loops to produce the following output:
+
+1
+22
+333
+4444
+55555
+666666
+7777777
+*/
+for(int i = 1; i <= 7; i++) {
+for(int j = i; j >= 1; j--) {
+System.out.print(i);
+}
+System.out.println();
+}
