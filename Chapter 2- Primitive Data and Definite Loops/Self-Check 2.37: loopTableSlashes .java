5765287a42f3273204ce5d1555ccda0ec62d188d
Mahdi Najjarian <mnajarian@gmail.com>
2018-01-24 12:11:48 +0200

    Create Self-Check 2.37: loopTableSlashes .java

---
+++ Chapter 2- Primitive Data and Definite Loops/Self-Check 2.37: loopTableSlashes .java
@@ -0,0 +1,18 @@
+/*Consider the following pattern of output consisting of 6 lines of repeated characters \, !, and /. Fill in the following equation for the number of each character relative to the line number for each type of character. We suggest that you write a table that lists the number of each character on each line that you can use to help find the equations from the output, as described in Chapter 2.
+
+!!!!!!!!!!!!!!!!!!!!!!
+\\!!!!!!!!!!!!!!!!!!//
+\\\\!!!!!!!!!!!!!!////
+\\\\\\!!!!!!!!!!//////
+\\\\\\\\!!!!!!////////
+\\\\\\\\\\!!//////////
+Compute the value of A, B, C, and D in the equations below. Assume that the lines are numbered from 1 through 6.
+
+number of \ = A * line + B
+number of ! = C * line + D
+*/
+
+A	2
+B	-2
+C	-4
+D	26
