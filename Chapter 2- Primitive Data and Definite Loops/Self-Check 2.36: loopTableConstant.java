a87c3efa9f3725b9763afa2a92ae8c4ab9c95d93
Mahdi Najjarian <mnajarian@gmail.com>
2018-01-24 12:10:28 +0200

    Create Self-Check 2.36: loopTableConstant.java

---
+++ Chapter 2- Primitive Data and Definite Loops/Self-Check 2.36: loopTableConstant.java
@@ -0,0 +1,7 @@
+/*Assume that you have a variable called line that will take on the values 1, 2, 3, 4, and so on, and a class constant named 
+SIZE that takes two values. You are going to formulate expressions in terms of line and SIZE that will yield different 
+sequences of numbers of characters. The first row is filled in for you as an example. Fill in the solution boxes below, 
+indicating an expression that will generate each sequence. Your answers must be in the exact 
+format 'coefficient1 * line + (coefficient2 * SIZE + offset)' in that order to be counted as correct.*/
+1. 4 * line + (3 * SIZE)
+2. line - (2 * SIZE) + 3
