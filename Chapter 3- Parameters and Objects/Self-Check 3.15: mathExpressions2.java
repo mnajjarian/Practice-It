64c888b435a595069f59bb271ca1dd3a314e9796
Mahdi Najjarian <mnajarian@gmail.com>
2018-01-27 17:25:43 +0200

    Create Self-Check 3.15: mathExpressions2.java

---
+++ Chapter 3- Parameters and Objects/Self-Check 3.15: mathExpressions2.java
@@ -0,0 +1,29 @@
+/*Write the result of each expression. Assume that each expression follows the ones before it, so that the code on a previous 
+line affects what would happen on the next line. Note that a variable's value changes only if you reassign it using 
+the = operator. (Make sure to write a value of the proper type, such as 12.0 rather than 12 for a double.)
+
+
+double grade = 2.7;
+Math.round(grade);                                // a) grade =
+grade = Math.round(grade);                        // b) grade =
+​
+double min = Math.min(grade, Math.floor(2.9));    // c) min =
+​
+double x = Math.pow(2, 4);                        // d) x =
+x = Math.sqrt(64);                                // e) x =
+​
+int count = 25;
+Math.sqrt(count);                                 // f) count =
+count = (int) Math.sqrt(count);                   // g) count =
+​
+int a = Math.abs(Math.min(-1, -3));               // h) a =
+*/
+
+ a) grade =	  2.7
+ b) grade =	  3.0
+ c) min =	    2.0
+ d) x =	      16.0
+ e) x =	      8.0
+ f) count =	  25
+ g) count =	  5
+ h) a =	      3
