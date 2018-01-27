0b6e28c40cd5a5f796a102ebfcb821356eb67cb7
Mahdi Najjarian <mnajarian@gmail.com>
2018-01-27 17:31:06 +0200

    Create Self-Check 3.19: stringExpressions.java

---
+++ Chapter 3- Parameters and Objects/Self-Check 3.19: stringExpressions.java
@@ -0,0 +1,21 @@
+/*Assuming that the following variables have been declared,
+
+
+//       index 0123456789012345
+String str1 = "Frodo Baggins";
+String str2 = "Gandalf the GRAY";
+Evaluate the following expressions. Make sure to give a value of the appropriate type (such as including quotes around a 
+String or apostrophes around a char).*/
+
+
+ str1.length()	     13
+ str1.charAt(7)	     'a'
+ str2.charAt(0)	     'G'
+ str1.indexOf("o")	  2
+ str2.toUpperCase()	  "GANDALF THE GRAY"
+ str1.toLowerCase().indexOf("B")	   -1
+ str1.substring(4)	           "o Baggins"
+ str2.substring(3, 14)	       "dalf the GR"
+ str2.replace("a", "oo")	     "Goondoolf the GRAY"
+ str2.replace("gray", "white")	 "Gandalf the GRAY"
+ "str1".replace("r", "range")	   "strange1"
