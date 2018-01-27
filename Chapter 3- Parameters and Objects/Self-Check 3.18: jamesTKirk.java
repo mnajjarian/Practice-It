666095e3ea4e93e95931b28744332652127379a0
Mahdi Najjarian <mnajarian@gmail.com>
2018-01-27 17:28:57 +0200

    Create Self-Check 3.18: jamesTKirk.java

---
+++ Chapter 3- Parameters and Objects/Self-Check 3.18: jamesTKirk.java
@@ -0,0 +1,18 @@
+/*What output is produced by the following code?
+
+
+String first = "James";
+String last = "Kirk";
+String middle = "T.";
+System.out.println(last);
+System.out.println("My name is " + first);
+System.out.println(first + " " + last);
+System.out.println(last + ", " + first + " " + middle);
+System.out.println(middle + " is for Tiberius");
+*/
+	
+Kirk
+My name is James
+James Kirk
+Kirk, James T.
+T. is for Tiberius
