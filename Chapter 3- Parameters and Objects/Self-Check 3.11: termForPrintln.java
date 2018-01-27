37ad5bf3329fc9c0c55469995d4f65561a19b09e
Mahdi Najjarian <mnajarian@gmail.com>
2018-01-27 12:21:10 +0200

    Create Self-Check 3.11: termForPrintln.java

---
+++ Chapter 3- Parameters and Objects/Self-Check 3.11: termForPrintln.java
@@ -0,0 +1,10 @@
+/*
+The System.out.println command works on many different types of values, including ints and doubles. 
+What is the term for such a method?
+ a overload
+ b overwrite
+ c override
+ d overrate
+ e parameter
+  */
+ a overload
