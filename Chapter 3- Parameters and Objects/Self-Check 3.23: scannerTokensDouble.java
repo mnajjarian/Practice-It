0eea9fc9435f411f6a88b2c96a10c76e093bcf33
Mahdi Najjarian <mnajarian@gmail.com>
2018-01-27 17:36:21 +0200

    Create Self-Check 3.23: scannerTokensDouble.java

---
+++ Chapter 3- Parameters and Objects/Self-Check 3.23: scannerTokensDouble.java
@@ -0,0 +1,15 @@
+/*Given the following code fragment, describe what will happen when the user types each of the following values. If the code will read the value successfully, describe the value that will be stored in the variable money. If it will not, write exception . (Make sure to write a value of the proper type, such as 12.0 rather than 12 for a double.)
+
+
+Scanner console = new Scanner(System.in);
+System.out.print("How much money do you have? ");
+double money = console.nextDouble();*/
+
+ 34.50	      34.5
+ 6	          6.0
+ $25.00	      exception
+ million	    exception
+ 100*5	      exception
+ 600x000	    exception
+ none	        exception
+ 645	        645.0
