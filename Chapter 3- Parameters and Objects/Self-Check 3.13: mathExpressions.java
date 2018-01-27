48503c19dcf7bf99c676900ada4c46b77263646b
Mahdi Najjarian <mnajarian@gmail.com>
2018-01-27 17:22:26 +0200

    Create Self-Check 3.13: mathExpressions.java

---
+++ Chapter 3- Parameters and Objects/Self-Check 3.13: mathExpressions.java
@@ -0,0 +1,19 @@
+/*Evaluate the following expressions. Make sure to give a value of the appropriate type (such as including a decimal point 
+in a double).*/
+
+ Math.abs(-1.6)         	1.6
+ Math.abs(2 + -4)	       2
+ Math.pow(6, 2)	         36.0
+ Math.pow(5 / 2, 6)	     64.0
+ Math.ceil(9.1)	         10.0
+ Math.ceil(115.8)	       116.0
+ Math.max(7, 4)	         7
+ Math.min(8, 3 + 2)	     5
+ Math.min(-2, -5)	       -5
+ Math.sqrt(64)	         8.0
+ Math.sqrt(76 + 45)	     11.0
+ 100 + Math.log10(100)	 102.0
+ 13 + Math.abs(-7) - Math.pow(2, 3) + 5	                      17.0
+ Math.sqrt(16) * Math.max(Math.abs(-5), Math.abs(-3))	        20.0
+ 7 - 2 + Math.log10(1000) + Math.log(Math.pow(Math.E, 5))	    13.0
+ Math.max(18 - 5 + Math.random(), Math.ceil(4.6 * 3))	        14.0
