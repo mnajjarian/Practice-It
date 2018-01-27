8dca1e1a9b7c9cbbe32679a3efc07b4807d1dc1c
Mahdi Najjarian <mnajarian@gmail.com>
2018-01-27 12:07:28 +0200

    Create Self-Check 3.1: methodHeaderSyntax.java

---
+++ Chapter 3- Parameters and Objects/Self-Check 3.1: methodHeaderSyntax.java
@@ -0,0 +1,8 @@
+/*Which of the following is the correct syntax for a method header with parameters?
+ a public static (int x, int y) example() {
+ b public static void example(x, y) {
+ c public static void example(x: int, y: int) {
+ d public static void example(int x, int y) {
+ e public static void example(int x,y) {
+  */
+ d public static void example(int x, int y) {
