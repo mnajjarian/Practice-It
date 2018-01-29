ead19091af6cfd5b8fcdf8143c751f5dc8660743
Mahdi Najjarian <mnajarian@gmail.com>
2018-01-29 12:49:37 +0200

    Create Exercise 3.10: area .java

---
+++ Chapter 3- Parameters and Objects/Exercise 3.10: area .java
@@ -0,0 +1,6 @@
+/*Write a method named area that accepts the radius of a circle as a parameter and returns the area of a circle with that 
+radius. For example, the call area(2.0) should return 12.566370614359172. You may assume that the radius is non-negative.*/
+
+public static double area(double number) {
+    return Math.PI * number * number;
+}
