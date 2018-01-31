1fda352db7edf73cafd83674bd7d03919e8d13d7
Mahdi Najjarian <mnajarian@gmail.com>
2018-01-31 16:08:23 +0200

    Create Self-Check 3G.1: drawRectangleSyntax.java

---
+++ Supplement 3G- Graphics/Self-Check 3G.1: drawRectangleSyntax.java
@@ -0,0 +1,8 @@
+/*Which of the following is the correct syntax to draw a rectangle?
+ a. Graphics.drawRect(10, 20, 50, 30);
+ b.  g.drawRect(x = 10, y = 20, width = 50, height = 30);
+ c.  g.drawRect(10, 20, 50, 30);
+ d.  g.draw.rectangle(10, 20, 50, 30);
+ e.  Graphics g.drawRect(10, 20, 50, 30);*/
+ 
+  c.  g.drawRect(10, 20, 50, 30);
