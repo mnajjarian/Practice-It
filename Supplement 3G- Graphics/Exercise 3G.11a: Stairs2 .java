68ccad01b0cfeddff8108e02b619f1b67c305a8f
Mahdi Najjarian <mnajarian@gmail.com>
2018-01-31 16:26:30 +0200

    Create Exercise 3G.11a: Stairs2 .java

---
+++ Supplement 3G- Graphics/Exercise 3G.11a: Stairs2 .java
@@ -0,0 +1,15 @@
+/*Modify your Stairs program from the previous exercise to make a new class Stairs2 that draws the output shown below. Modify only the body of your loop.
+(You may want to make a new table to find the expressions for x, y, width, and height for each new output.)
+ 
+(You don't need to include any import statements at the top of your program.) (The next exercise is a modified version of this 
+program, so you can use the code you write here as a starting point for that exercise.)*/
+
+public class Stairs2 {
+    public static void main(String[] args) {
+        DrawingPanel panel = new DrawingPanel(110, 110);
+        Graphics g = panel.getGraphics();
+        for (int i = 1; i <= 10; i++) {
+            g.drawRect(5, 10 * i - 5, 110 - 10 * i, 10);
+        }
+    }
+}
