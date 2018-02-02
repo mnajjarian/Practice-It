2df2ab6a971abab988bd7d3b2e3b74794ec1edc1
Mahdi Najjarian <mnajarian@gmail.com>
2018-02-02 11:13:17 +0200

    Create Self-Check 4.10: javaIsAwesome.java

---
+++ Chapter 4- Conditional Execution/Self-Check 4.10: javaIsAwesome.java
@@ -0,0 +1,8 @@
+/*The following code is poorly structured. It contains redundant statements that unnecessarily occur multiple times. Factor out 
+redundant code from the following example by moving it out of the if/else statement, while preserving the same behavior and output.*/
+
+if (x < 30) {
+    x++;
+}
+    a = 2;
+    System.out.println("Java is awesome! " + x);
