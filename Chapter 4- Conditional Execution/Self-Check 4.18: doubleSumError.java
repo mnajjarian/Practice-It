183079d31c295762590d46bf315ca76bdf71f27e
Mahdi Najjarian <mnajarian@gmail.com>
2018-02-02 11:41:39 +0200

    Create Self-Check 4.18: doubleSumError.java

---
+++ Chapter 4- Conditional Execution/Self-Check 4.18: doubleSumError.java
@@ -0,0 +1,7 @@
+/*The expression (0.2 + 1.2 + 2.2 + 3.2) should equal 6.8, but in Java it does not. Why not?
+ a. The programmer forgot to cast the expression to type double, so the result is inaccurate.
+ b.  Real numbers are represented as imprecise floating-point numbers in the computer, and the limited precision led to a roundoff error.
+ c. Those numbers are integers in Java, so the sum is 6.
+ d. Java has a bug making it unable to add real numbers accurately, unlike other superior languages such as C++.
+ e. Java is unable to perform a + expression involving so many numbers, so the code does not compile.*/
+ b.  Real numbers are represented as imprecise floating-point numbers in the computer, and the limited precision led to a roundoff error.
