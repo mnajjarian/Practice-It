039038576750f97c30c8ded288511365d26ea454
Mahdi Najjarian <mnajarian@gmail.com>
2018-02-02 11:50:47 +0200

    Create Self-Check 4.25: printTriangleTypePreconditions.java

---
+++ Chapter 4- Conditional Execution/Self-Check 4.25: printTriangleTypePreconditions.java
@@ -0,0 +1,23 @@
+/*Consider a method printTriangleType that accepts three integer arguments representing the lengths of the sides of a triangle and 
+prints the type of triangle that these sides form.
+
+public static void printTriangleType(int a, int b, int c) {
+    ...
+}
+
+The three types are equilateral, isosceles, and scalene. An equilateral triangle has three sides of the same length, an isosceles 
+triangle has two sides that are the same length, and a scalene triangle has three sides of different lengths. However, certain 
+integer values (or combinations of values) would be illegal and could not represent the sides of an actual triangle. What are 
+these values? (In other words, how would you describe the precondition(s) of the printTriangleType method?)
+
+Which of the following are precondition(s) that are appropriate for this method? Check all that apply.
+
+ a. The three side lengths must be positive integers.
+ b. No side's length can be greater than 180.
+ c. All sides must have the same length.
+ d. No side's length may exceed the sum of any two other side lengths.
+ e. No two sides' lengths can be the same.
+ f. The sum of the sides' lengths must be an even number.*/
+ 
+ a. The three side lengths must be positive integers.
+ d. No side's length may exceed the sum of any two other side lengths.
