6ebb879bc5a091ab813e4f1f4b46dc1b35b0a101
Mahdi Najjarian <mnajarian@gmail.com>
2018-02-02 11:04:28 +0200

    Create Self-Check 4.2: logicExpressions1.java

---
+++ Chapter 4- Conditional Execution/Self-Check 4.2: logicExpressions1.java
@@ -0,0 +1,16 @@
+/*Given the following variable declarations:
+
+int x = 4;
+int y = -3;
+int z = 4;
+What are the results of the following relational expressions?*/
+
+ x == 4	       true
+ x == y	       false
+ x == z	       true
+ y == z	       false
+ x + y > 0	   true
+ x - z != 0	   false
+ y * y <= z	   false
+ y / y == 1	   true
+ x * (y + 2) > y - (y + z) * 2	   true
