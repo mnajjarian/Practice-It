c59473068d8117439a6568b64d8f078742db7b02
Mahdi Najjarian <mnajarian@gmail.com>
2018-02-02 11:05:42 +0200

    Create Self-Check 4.3: ifStatementSyntax.java

---
+++ Chapter 4- Conditional Execution/Self-Check 4.3: ifStatementSyntax.java
@@ -0,0 +1,8 @@
+/*Which of the following if statement headers uses the correct syntax?
+ a. if (x => y) {
+ b. if (x == y) {
+ c. if [x == 10] {
+ d. if (x equals 42) {
+ e. if x = 10 then {
+ */
+  b. if (x == y) {
