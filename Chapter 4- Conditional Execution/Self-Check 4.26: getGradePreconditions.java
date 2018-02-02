fcfeb459d29afbd2f8225dba0489a57c7a8eb6e3
Mahdi Najjarian <mnajarian@gmail.com>
2018-02-02 11:52:46 +0200

    Create Self-Check 4.26: getGradePreconditions.java

---
+++ Chapter 4- Conditional Execution/Self-Check 4.26: getGradePreconditions.java
@@ -0,0 +1,17 @@
+/*Consider a method getGrade that accepts an integer representing a student's grade percentage in a course and returns that student's numerical course grade:
+
+public static double getGrade(int percentage) {
+    ...
+}
+
+The grade can be between 0.0 (failing) and 4.0 (perfect).
+
+Which of the following are precondition(s) that are appropriate for this method? Check all that apply.
+
+ a. The parameter's value must be greater than or equal to 0.
+ b. The parameter's value must be between 0.0 and 4.0.
+ c. The parameter can be an integer or a real number.
+ d. The parameter's value must be less than or equal to 100.
+ e. The parameter must be a letter grade of 'A', 'B', 'C', or 'D'.*/
+  a. The parameter's value must be greater than or equal to 0.
+  d. The parameter's value must be less than or equal to 100.
