fb335d089185f3a0682abce55351256dcbccc692
Mahdi Najjarian <mnajarian@gmail.com>
2018-02-02 11:06:29 +0200

    Create Self-Check 4.4: Oops4-errors.java

---
+++ Chapter 4- Conditional Execution/Self-Check 4.4: Oops4-errors.java
@@ -0,0 +1,25 @@
+/*The following program contains 7 errors. Correct the errors and submit a working version of the program. The corrected version 
+of the program should produce the following output:
+
+a is the smallest!
+*/
+public class Oops4 {
+    public static void main(String[] args) {
+        int a = 7, b = 42;
+        minimum(a, b);
+        int smaller = 0;
+        if (smaller <= a) {
+            System.out.println("a is the smallest!");
+        }
+    }
+
+    public static void minimum(int a, int b) {
+        if (a < b) {
+            int smaller = a;
+        } else if(a >= b) {
+            int smaller = b;
+        } else {
+        int smaller;
+    }
+}
+}
