b5146ebb2db6563f782e8afc23e562fca290ee8f
Mahdi Najjarian <mnajarian@gmail.com>
2018-02-02 11:43:54 +0200

    Create  Self-Check 4.20: charMystery.java

---
+++ Chapter 4- Conditional Execution/Self-Check 4.20: charMystery.java
@@ -0,0 +1,21 @@
+/*What output is produced by the following program?
+
+public class CharMystery {
+    public static void printRange(char startLetter, char endLetter) {
+        for (char letter = startLetter; letter <= endLetter; letter++) {
+            System.out.print(letter);
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        printRange('e', 'g');
+        printRange('n', 's');
+        printRange('z', 'a');
+        printRange('q', 'r');
+    }
+}*/
+
+ printRange('e', 'g');	efg
+ printRange('n', 's');	nopqrs
+ printRange('z', 'a');	
+ printRange('q', 'r');	qr
