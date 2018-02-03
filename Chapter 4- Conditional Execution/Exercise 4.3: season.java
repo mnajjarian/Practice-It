c7406f3c5cbd8a2ddfb7ed42de9d3ed94c40dc97
Mahdi Najjarian <mnajarian@gmail.com>
2018-02-03 23:02:24 +0200

    Create Exercise 4.3: season.java

---
+++ Chapter 4- Conditional Execution/Exercise 4.3: season.java
@@ -0,0 +1,21 @@
+/*Write a method named season that takes two integers as parameters representing a month and day and that returns a String indicating 
+the season for that month and day. Assume that months are specified as an integer between 1 and 12 (1 for January, 2 for February, 
+and so on) and that the day of the month is a number between 1 and 31.
+
+If the date falls between 12/16 and 3/15, you should return "Winter". If the date falls between 3/16 and 6/15, you should return 
+"Spring". If the date falls between 6/16 and 9/15, you should return "Summer". And if the date falls between 9/16 and 12/15, 
+you should return "Fall".*/
+
+public static String season(int month, int day) {
+		if(month < 3 || month == 3 && day <= 15 || (month == 12 && day >= 16)) {
+				return "Winter";
+			} else if ((month < 6 && month > 3) || (month == 3 && day >= 16) ||
+					(month == 6 && day <= 15)) {
+					return "Spring";
+				} else if((month > 6 && month < 9) || (month == 6 && day >= 16) ||
+						(month == 9 && day <= 15)) {
+						return "Summer";
+					} else {
+						return "Fall";
+		}
+		}
