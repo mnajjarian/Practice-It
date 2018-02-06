4d67d07b5b517e78957868fe5d14bb393fe34eb8
Mahdi Najjarian <mnajarian@gmail.com>
2018-02-07 00:33:56 +0200

    Create Self-Check 7.2: numbersElements.java

---
+++ Chapter 7- Arrays/Self-Check 7.2: numbersElements.java
@@ -0,0 +1,7 @@
+/*
+-What expression should be used to access the first element of an array of integers called numbers?	
+-What expression should be used to access the last element of numbers, assuming it contains 10 elements?	
+-What expression can be used to access its last element, regardless of its length?*/
+ numbers[0]
+ numbers[9]
+ numbers[numbers.length-1]
