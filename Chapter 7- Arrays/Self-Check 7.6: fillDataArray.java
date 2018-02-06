c0a2819ef64d1f34c5ec799320c0132fdd3f004c
Mahdi Najjarian <mnajarian@gmail.com>
2018-02-07 00:38:27 +0200

    Create Self-Check 7.6: fillDataArray.java

---
+++ Chapter 7- Arrays/Self-Check 7.6: fillDataArray.java
@@ -0,0 +1,22 @@
+/*Fill in the array with the values that would be stored after the code executes:
+
+
+int[] data = new int[8];
+data[0] = 3;
+data[7] = -18;
+data[4] = 5;
+data[1] = data[0];
+​
+int x = data[4];
+data[4] = 6;
+data[x] = data[0] * data[1];
+*/
+
+data[0] = 3
+data[1]	= 3
+data[2]	= 0
+data[3]	= 0
+data[4]	= 6
+data[5]	= 9
+data[6]	= 0
+data[7]	= -18
