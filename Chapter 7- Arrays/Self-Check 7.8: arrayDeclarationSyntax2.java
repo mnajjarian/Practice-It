05c9f9780475830dedc63c55ecb7ab4404909c82
Mahdi Najjarian <mnajarian@gmail.com>
2018-02-07 00:40:33 +0200

    Create Self-Check 7.8: arrayDeclarationSyntax2.java

---
+++ Chapter 7- Arrays/Self-Check 7.8: arrayDeclarationSyntax2.java
@@ -0,0 +1,9 @@
+/*
+Which of the following choices is the correct syntax for quickly declaring/initializing an array of six integers to store a 
+particular list of values?
+  a. int[] a = {17, -3, 42, 5, 9, 28};
+  b. int a {17, -3, 42, 5, 9, 28};
+  c. int[6] a = {17, -3, 42, 5, 9, 28};
+  d. int[] a = new {17, -3, 42, 5, 9, 28} [6];
+  e. int[] a = new int[6] {17, -3, 42, 5, 9, 28};*/
+  int[] a = {17, -3, 42, 5, 9, 28};
