1a7b73b14c7b5c0f505da470ce387093c7ff4ef8
Mahdi Najjarian <mnajarian@gmail.com>
2018-02-07 00:41:34 +0200

    Create Self-Check 7.9: declareArray.java

---
+++ Chapter 7- Arrays/Self-Check 7.9: declareArray.java
@@ -0,0 +1,3 @@
+/*Write a piece of code that declares an array variable named data with the elements 7, -1, 13, 24, and 6. 
+Use only one statement to initialize the array.*/
+int[] data = {7, -1, 13, 24, 6};
