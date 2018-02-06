071ee243c16b05f4d39514e24528224e7b92efb4
Mahdi Najjarian <mnajarian@gmail.com>
2018-02-07 00:32:06 +0200

    Create Self-Check 7.1: arrayDeclarationSyntax.java

---
+++ Chapter 7- Arrays/Self-Check 7.1: arrayDeclarationSyntax.java
@@ -0,0 +1,9 @@
+/*
+Which of the following choices is the correct syntax for declaring/initializing an array of ten integers?
+ a. int[10] a = new int[10];
+ b. int a[10];
+ c. int[] a = new int[10];
+ d. []int a = [10]int;
+ e. int a[10] = new int[10];
+  */
+ int[] a = new int[10];
