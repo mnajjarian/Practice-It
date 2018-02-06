59720fe332c47c379236cd57de42d7ca81cc08a5
Mahdi Najjarian <mnajarian@gmail.com>
2018-02-07 00:34:50 +0200

    Create Self-Check 7.3: dataArray.java

---
+++ Chapter 7- Arrays/Self-Check 7.3: dataArray.java
@@ -0,0 +1,10 @@
+/*Write code that creates an array of integers named data of size 5 with the following contents:
+
+[27, 51, 33, -1, 101]
+*/
+int[] data = new int[5];
+data[0] = 27;
+data[1] = 51;
+data[2] = 33;
+data[3] = -1;
+data[4] = 101;
