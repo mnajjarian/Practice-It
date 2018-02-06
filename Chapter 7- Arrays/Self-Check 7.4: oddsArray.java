0dad079790fa4c8b9c9864e8e68259d224578ef8
Mahdi Najjarian <mnajarian@gmail.com>
2018-02-07 00:35:42 +0200

    Create Self-Check 7.4: oddsArray.java

---
+++ Chapter 7- Arrays/Self-Check 7.4: oddsArray.java
@@ -0,0 +1,6 @@
+/*Write code that creates an array named odds and stores all odd numbers between -6 and 38 into it using a for loop. 
+Make the array's size exactly large enough to store the numbers.*/
+int[] odds = new int[22];
+for(int i = 0; i < 22; i++) {
+        odds[i] = i * 2 - 5;
+}
