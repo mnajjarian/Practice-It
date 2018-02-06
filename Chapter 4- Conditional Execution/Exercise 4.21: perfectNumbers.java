f8c334e76b627abad2b9c437224f5f4fd1bd80b2
Mahdi Najjarian <mnajarian@gmail.com>
2018-02-06 11:32:10 +0200

    Create Exercise 4.21: perfectNumbers.java

---
+++ Chapter 4- Conditional Execution/Exercise 4.21: perfectNumbers.java
@@ -0,0 +1,26 @@
+/*A "perfect number" is a positive integer that is the sum of all its proper factors (that is, factors including 1 but not the number itself).
+The first two perfect numbers are 6 and 28, since 1+2+3=6 and 1+2+4+7+14=28. Write a static method perfectNumbers that takes an
+integer max as an argument and prints out all perfect numbers that are less than or equal to max.
+
+Here is the console output from a call to perfectNumbers(6):
+
+Perfect numbers up to 6: 6 
+Here is the console output from a call to perfectNumbers(500):
+
+Perfect numbers up to 500: 6 28 496 
+*/
+public static void perfectNumbers(int max) {
+		String k = "";
+		int sum = 0;
+		for(int i = 1; i <= max; i++) {
+			for(int j = 1; j < i; j++) 
+				if(i % j == 0) {
+					sum += j;
+				}
+				if(sum == i) {
+					 k+= i + " ";
+				}
+            sum = 0;
+        }
+    System.out.println("Perfect numbers up to " + max + ": " + k);
+}
