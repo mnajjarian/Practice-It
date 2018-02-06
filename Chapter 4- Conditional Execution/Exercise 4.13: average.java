9d2ebd49e9d5b8e7992d38202c1f23d6b0246fff
Mahdi Najjarian <mnajarian@gmail.com>
2018-02-06 11:21:54 +0200

    Create Exercise 4.13: average.java

---
+++ Chapter 4- Conditional Execution/Exercise 4.13: average.java
@@ -0,0 +1,5 @@
+/*Write a method called average that takes two integers as parameters and returns the average of the two integers.*/
+public static double average(int a, int b) {
+    double average = (double) (a + b) / 2;
+    return average;
+}
