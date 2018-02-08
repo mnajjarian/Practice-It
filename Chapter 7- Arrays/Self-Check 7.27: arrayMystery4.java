636c75a83ad68a2f61d424082cf33e909a26526a
Mahdi Najjarian <mnajarian@gmail.com>
2018-02-09 00:54:38 +0200

    Create Self-Check 7.27: arrayMystery4.java

---
+++ Chapter 7- Arrays/Self-Check 7.27: arrayMystery4.java
@@ -0,0 +1,21 @@
+/*Consider the following method:
+
+
+public static int arrayMystery4(int[] list) {
+    int x = 0;
+    for (int i = 1; i < list.length; i++) {
+        int y = list[i] - list[0];
+        if (y > x) {
+            x = y;
+        }
+    }
+    return x;
+}
+For each array below, indicate what value would be returned if the method mystery were called and passed that array as its parameter.*/
+
+
+ {5}	0
+ {3, 12}	9
+ {4, 2, 10, 8}	6
+ {1, 9, 3, 5, 7}	8
+ {8, 2, 10, 4, 10, 9}	2
