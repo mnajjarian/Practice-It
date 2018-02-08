2a21044f430af055c4124c40904a6216178ccb78
Mahdi Najjarian <mnajarian@gmail.com>
2018-02-09 00:50:07 +0200

    Create Self-Check 7.23: arrayCodeTracing4.java

---
+++ Chapter 7- Arrays/Self-Check 7.23: arrayCodeTracing4.java
@@ -0,0 +1,18 @@
+/*What are the values of the elements in the array numbers after the following code is executed?
+
+
+int[] numbers = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+for (int i = 1; i < 10; i++) {
+    numbers[i] = numbers[i - 1];
+}*/
+
+ numbers[0]	10
+ numbers[1]	10
+ numbers[2]	10
+ numbers[3]	10
+ numbers[4]	10
+ numbers[5]	10
+ numbers[6]	10
+ numbers[7]	10
+ numbers[8]	10
+ numbers[9]	10
