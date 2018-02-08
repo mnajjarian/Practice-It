88b6069e6a3270a9da01673e04ec8bfdb26c4b3e
Mahdi Najjarian <mnajarian@gmail.com>
2018-02-08 10:36:36 +0200

    Create Self-Check 7.18: arraySwap.java

---
+++ Chapter 7- Arrays/Self-Check 7.18: arraySwap.java
@@ -0,0 +1,7 @@
+/*Why does a method to swap two array elements work correctly when a method to swap two integer values does not?
+ a. Unlike integers, arrays are objects and use reference semantics.
+ b. Because of a bug in Java.
+ c. Because arrays use more memory than primitive values.
+ d. Because arrays are slower and more inefficient than primitives, so swapping is possible.
+ e. Changes to an array parameter's elements will not be seen in the original array by the caller.*/
+  a. Unlike integers, arrays are objects and use reference semantics.
