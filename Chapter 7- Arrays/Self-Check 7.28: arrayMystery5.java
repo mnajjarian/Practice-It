15444ac686175effc981943032082a41a9ffeb3b
Mahdi Najjarian <mnajarian@gmail.com>
2018-02-09 00:56:36 +0200

    Create Self-Check 7.28: arrayMystery5.java

---
+++ Chapter 7- Arrays/Self-Check 7.28: arrayMystery5.java
@@ -0,0 +1,16 @@
+/*Consider the following method:
+
+
+public static void arrayMystery5(int[] nums) {
+    for (int i = 0; i < nums.length - 1; i++) {
+        if (nums[i] > nums[i + 1]) {
+            nums[i + 1]++;
+        }
+    }
+}
+For each array below, indicate what the array's contents would be after the method mystery were called and passed that array as its parameter.*/
+ {8}	                       [8]
+ {14, 7}	                   [14, 8]
+ {7, 1, 3, 2, 0, 4}	         [7, 2, 3, 3, 1, 4]
+ {10, 8, 9, 5, 5}	           [10, 9, 9, 6, 6]
+ {12, 11, 10, 10, 8, 7}	     [12, 12, 11, 11, 9, 8]
