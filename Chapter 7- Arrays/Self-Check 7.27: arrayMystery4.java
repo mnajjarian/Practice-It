2de1e41d2a1beb0a7c4eca1f916cb551c75c032a
Mahdi Najjarian <mnajarian@gmail.com>
2018-02-09 00:55:21 +0200

    Update Self-Check 7.27: arrayMystery4.java

--- Chapter 7- Arrays/Self-Check 7.27: arrayMystery4.java
+++ Chapter 7- Arrays/Self-Check 7.27: arrayMystery4.java
@@ -14,8 +14,8 @@ public static int arrayMystery4(int[] list) {
 For each array below, indicate what value would be returned if the method mystery were called and passed that array as its parameter.*/
 
 
- {5}	0
- {3, 12}	9
- {4, 2, 10, 8}	6
- {1, 9, 3, 5, 7}	8
+ {5}                    0
+ {3, 12}	            9
+ {4, 2, 10, 8}	        6
+ {1, 9, 3, 5, 7}      	8
  {8, 2, 10, 4, 10, 9}	2
