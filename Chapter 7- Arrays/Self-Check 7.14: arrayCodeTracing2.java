6fbff3160bce2264709e0d43618b33f40f95bf1f
Mahdi Najjarian <mnajarian@gmail.com>
2018-02-08 10:31:52 +0200

    Create Self-Check 7.14: arrayCodeTracing2.java

---
+++ Chapter 7- Arrays/Self-Check 7.14: arrayCodeTracing2.java
@@ -0,0 +1,14 @@
+/*Fill in the array with the values that would be stored after the code executes:
+
+
+int[] list = {2, 18, 6, -4, 5, 1};
+for (int i = 0; i < list.length; i++) {
+    list[i] = list[i] + (list[i] / list[0]);
+}*/
+
+ list[0] =	3
+ list[1] =	24
+ list[2] =	8
+ list[3] =	-5
+ list[4] =	6
+ list[5] =	1
