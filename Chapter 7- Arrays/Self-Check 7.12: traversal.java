1cad48f6e774eda08754d34d1743673fa4e8320e
Mahdi Najjarian <mnajarian@gmail.com>
2018-02-08 10:29:24 +0200

    Create Self-Check 7.12: traversal.java

---
+++ Chapter 7- Arrays/Self-Check 7.12: traversal.java
@@ -0,0 +1,17 @@
+/*Which of the following best describes an array traversal?
+ a. Constructing a new empty array.
+ b. Changing the elements from positive to negative.
+ c. Changing the elements into the opposite order.
+ d. A sequential processing of each of an array's elements.
+ e.  Deleting values from an array.*/
+ d. A sequential processing of each of an array's elements.
+  
+/*Which of the following are examples of problems that can be solved by traversing an array?
+ a. Searching an array for a given value.
+ b. Printing an array's elements.
+ c. Swapping the first and last values of an array.
+ d. Comparing two arrays for equality.
+ e. Examining the value of the first element of the array.*/
+ a. Searching an array for a given value.
+ b. Printing an array's elements.
+ d. Comparing two arrays for equality.
