354e7a1dd1574a4b5ef7bf28ddd4308d7ab1b398
Mahdi Najjarian <mnajarian@gmail.com>
2018-02-11 20:58:46 +0200

    Create Self-Check 7.31: arrayCodeTracing2d.java

---
+++ Chapter 7- Arrays/Self-Check 7.31: arrayCodeTracing2d.java
@@ -0,0 +1,21 @@
+/*What are the values of the elements in the array numbers after the following code is executed?
+
+
+int[][] numbers = new int[3][4];
+for (int r = 0; r < numbers.length; r++) {
+    for (int c = 0; c < numbers[0].length; c++) {
+        numbers[r][c] = r + c;
+    }
+}*/
+ numbers[0][0]	0
+ numbers[0][1]	1
+ numbers[0][2]	2
+ numbers[0][3]	3
+ numbers[1][0]	1
+ numbers[1][1]	2
+ numbers[1][2]	3
+ numbers[1][3]	4
+ numbers[2][0]	2
+ numbers[2][1]	3
+ numbers[2][2]	4
+ numbers[2][3]	5
