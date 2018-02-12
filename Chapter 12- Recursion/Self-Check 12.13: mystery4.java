a6df3b21776d5cc3b7df61a2479bd345f2baa891
Mahdi Najjarian <mnajarian@gmail.com>
2018-02-12 23:32:00 +0200

    Create Self-Check 12.13: mystery4.java

---
+++ Chapter 12- Recursion/Self-Check 12.13: mystery4.java
@@ -0,0 +1,15 @@
+/*For each call to the following method, indicate what value is returned:
+
+public int mystery4(int x, int y) {
+    if (x < y) {
+        return x;
+    } else {
+        return mystery4(x - y, y);
+    }
+}*/
+
+ mystery4(6, 13)	6
+ mystery4(14, 10)	4
+ mystery4(37, 10)	7
+ mystery4(8, 2)	  0
+ mystery4(50, 7)	1
